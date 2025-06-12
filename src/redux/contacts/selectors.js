5c3f2924bf6d27bb0291fe1fb42346b838a0e1c3
Mürsel ŞEN <murselsen_35@hotmail.com>
2025-06-12 10:08:45 +0300

    Fix selector for filtered contacts to correctly filter by name and phone

--- src/redux/contacts/selectors.js
+++ src/redux/contacts/selectors.js
@@ -2,3 +2,20 @@ export const selectContacts = (state) => state.contants.items;
 export const selectIsLoading = (state) => state.contants.isLoading;
 export const selectError = (state) => state.contants.error;
 export const selectFilteredContacts = (state) => {
+  const contacts = selectContacts();
+  const filterType = state.filters.type.toLowerCase();
+  const filterValue = state.filters.value.toLowerCase();
+
+  switch (filterType) {
+    case "name":
+      return contacts.filter((contact) =>
+        contact.name.toLowerCase().includes(filterValue)
+      );
+    case "phone":
+      return contacts.filter((contact) =>
+        contact.phone.toLowerCase().includes(filterValue)
+      );
+    default:
+      return contacts;
+  }
+};

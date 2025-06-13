b1152ce243a93a502b1a71bb3b57ae0518f40071
Mürsel ŞEN <murselsen_35@hotmail.com>
2025-06-13 14:35:25 +0300

    Refactor Home component to utilize Redux for user state and update styles

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,9 +1,31 @@
 import React from "react";
-import css from "./Page.module.css";
+import pageCss from "./Page.module.css";
+import css from "./styles/Home.module.css";
+
+// Redux
+import { useSelector } from "react-redux";
+
+import { selectAuthUser } from "../redux/auth/selectors";
 const Home = () => {
+  const user = useSelector(selectAuthUser);
   return (
-    <div className={css.Container}>
-      <h4> Home Page </h4>
+    <div className={pageCss.Container}>
+      <div className={css.Home}>
+        <div className={css.HomeRow}>
+          <h1 className={css.Title}>
+            {user?.name ? (
+              <h1>Welcome, {user.name}! 👋</h1>
+            ) : (
+              <h1>Welcome to the PhoneBook Application 📞</h1>
+            )}
+          </h1>
+          <p className={css.Description}>
+            {user?.name
+              ? 'Click on the "Contacts" tab above to browse your phone book.'
+              : "Please login or register to continue."}
+          </p>
+        </div>
+      </div>
     </div>
   );
 };
